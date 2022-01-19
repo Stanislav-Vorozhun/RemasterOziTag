e65cb824f491d0ea8b3ed991c8731297b6b1c73c
Stanislav Vorozhun <stanislav.vorozhun@gmail.com>
2022-01-19 22:05:28 +0300

    add placeholder

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let searchField = document.querySelector('.searchField')
+
+
+
+// document.querySelector('select').remove()
+searchField.setAttribute('placeholder', "Код ОКРБ или наименование закупаемой продукции")
+searchField.addEventListener('click', () => {
+    console.log('clck')
+})
