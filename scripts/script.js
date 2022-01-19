949d9ae2f30d5c7850555c2bc03bff79f91a7b73
Stanislav Vorozhun <stanislav.vorozhun@gmail.com>
2022-01-19 23:13:46 +0300

    add search and catch func

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,31 @@
-let searchField = document.querySelector('.searchField')
+let searchField = document.querySelector('.searchField'),
+    select = document.querySelector('select'),
+    optionInSelect = document.querySelectorAll('option')
+
+
+// const products = {
+//     "A": "[A] - Продукция сельского хозяйства, лесного хозяйства рыбоводства и рыболовства",
+//     "О": "[О] - Услуги в области государственного управления и обороны, предоставляемые обществу в целом; услуги по обязательному социальному страхованию",
+//     "84": {
+//         "84": "[84] - Услуги в области государственного управления и обороны, предоставляемые обществу в целом; услуги по обязательному социальному страхованию",
+//         "84.1": {
+//             "84.1": "[84.1] - Услуги в области государственного управления общего характера и социально-экономической сфере",
+//             "84.11": {
+//                 "84.11": "[84.11] - Услуги в области государственного управления общего характера",
+//                 "84.11.11": {
+//                     "84.11.11": "[84.11.11] - Услуги в области исполнительной и законодательной деятельности",
+//                     "84.11.11.100": {
+//                         "84.11.11.100": "[84.11.11.100] - Услуги центральных органов исполнительной и законодательной власти",
+//                         "84.11.11.200": "[84.11.11.200] - Услуги органов управления и самоуправления областного территориального уровня",
+//                         "84.11.11.300": "[84.11.11.300] - Услуги органов управления и самоуправления базового территориального уровня"
+//                     }
+//                 },
+//                 "84.11.12": "[84.11.12] - Услуги в области государственного управления общего характера"
+//             }
+//         }
+//     }
+// }
+
 
 
 
@@ -6,4 +33,23 @@ let searchField = document.querySelector('.searchField')
 searchField.setAttribute('placeholder', "Код ОКРБ или наименование закупаемой продукции")
 searchField.addEventListener('click', () => {
     console.log('clck')
+
 })
+
+function optionIsSelected() {
+    for (var i = 0; i < optionInSelect.length; i++) {
+        if (optionInSelect[i].selected)
+            console.log(optionInSelect[i])
+    }
+}
+
+function catchOptionsData() {
+    let optionsData = []
+    for (var i = 0; i < optionInSelect.length; i++) {
+        optionsData.push(optionInSelect[i].text)
+    }
+    console.log(optionsData)
+}
+
+optionIsSelected()
+catchOptionsData()
