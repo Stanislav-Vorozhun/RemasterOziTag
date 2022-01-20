5517a32105dcdda4260111f447231e814d983b49
Stanislav Vorozhun <stanislav.vorozhun@gmail.com>
2022-01-20 18:48:19 +0300

    refact es6

--- scripts/script.js
+++ scripts/script.js
@@ -11,37 +11,63 @@ searchField.addEventListener('click', () => {
 })
 
 
-function isSelected(arr1, arr2) {
+// переделать
+// function isSelected(arr1, arr2) {
+//     for (let i = 0; i < arr1.length; i++) {
+//         if (arr1[i].selected) {
+//             var a = i
+//             console.log(arr1[i])
+//             for (var j = 0; j < arr2.length; j++) {
+//                 if (a === j) {
+//                     arr2[j].classList.toggle('selected')
+//                     console.log(j + ' add cls')
+//                 }
+//                 if (arr2[j].classList.contains('selected')) {
+//                     console.log('я ' + j + ' и у меня есть этот класс')
+//                     document.querySelector('.customChexboxInput').checked = true
+//                 }
+//             }
+//         }
+//     }
+//     return
+// }
+
+let isSelected = (arr1, arr2) => {
     for (let i = 0; i < arr1.length; i++) {
         if (arr1[i].selected) {
             var a = i
             console.log(arr1[i])
             for (var j = 0; j < arr2.length; j++) {
-                if(a === j){
-                    arr2[i].classList.toggle('selected')
-                    console.log( j + ' add cls')
+                if (a === j) {
+                    arr2[j].classList.toggle('selected')
+                    console.log(j + ' add cls')
                 }
-                    
+                if (arr2[j].classList.contains('selected')) {
+                    console.log('я ' + j + ' и у меня есть этот класс')
+                    document.querySelector('.customChexboxInput').checked = true
                 }
+            }
         }
-            
     }
-        
-    return
-
 }
 
-function catchOptionsData(arr) {
+// function catchOptionsData(arr) {
 
+//     for (let i = 0; i < optionInSelect.length; i++) {
+//         arr.push(optionInSelect[i].text)
+
+//     }
+//     return
+
+// }
+
+let catchOptionsData = (arr) => {
     for (let i = 0; i < optionInSelect.length; i++) {
         arr.push(optionInSelect[i].text)
-
     }
-    return
-
 }
 
-function generateCustomOptions(items) {
+let generateCustomOptions = (items) => {
     items.map((text) => (
         customOption = document.createElement('div'),
         customOption.className = 'customOption',
@@ -63,9 +89,51 @@ function generateCustomOptions(items) {
 
         document.querySelector('.dropDown').append(customOption),
         customOption.append(customCheckBoxInput, customCheckBoxLabel, customOptionTitle)
-
     ))
+
 }
+
+let addChecker = (items) => {
+    for (let i = 0; i < items.length; i++) {
+        items[i].addEventListener('click', () => {
+            child = items[i].querySelector('input')
+            if (!items[i].classList.contains('selected')) {
+                items[i].classList.toggle('selected')
+                child.checked = true
+            }
+            else {
+                child.checked = false
+                items[i].classList.toggle('selected')
+            }
+        })
+    }
+}
+
+// function generateCustomOptions(items) {
+//     items.map((text) => (
+//         customOption = document.createElement('div'),
+//         customOption.className = 'customOption',
+
+
+//         customCheckBoxInput = document.createElement('input'),
+//         customCheckBoxInput.setAttribute('type', 'checkbox'),
+//         customCheckBoxInput.className = 'customChexboxInput',
+//         customCheckBoxInput.id = 'customChexboxInput' + Math.floor(Math.random() * 999),
+
+//         customCheckBoxLabel = document.createElement('label'),
+//         customCheckBoxLabel.className = 'customCheckBoxLabel',
+//         customCheckBoxLabel.setAttribute('for', `${customCheckBoxInput.id}`),
+
+//         customOptionTitle = document.createElement('p'),
+//         customOptionTitle.className = 'customChexboxTitle',
+//         customOptionTitle.innerHTML = text,
+
+
+//         document.querySelector('.dropDown').append(customOption),
+//         customOption.append(customCheckBoxInput, customCheckBoxLabel, customOptionTitle)
+
+//     ))
+// }
 
 
 catchOptionsData(optionsDataText)
@@ -74,3 +142,27 @@ generateCustomOptions(optionsDataText)
 newCustomOptions = document.querySelectorAll('.customOption')
 
 isSelected(optionInSelect, newCustomOptions)
+
+
+allCustomOptions = document.querySelectorAll('.customOption')
+
+// function addChecker(items) {
+//     for (let i = 0; i < items.length; i++) {
+//         items[i].addEventListener('click', () => {
+//             child = items[i].querySelector('input')
+//             if (!items[i].classList.contains('selected')) {
+//                 items[i].classList.toggle('selected')
+//                 child.checked = true
+//             }
+//             else {
+//                 child.checked = false
+//                 items[i].classList.toggle('selected')
+//             }
+//         })
+//     }
+
+// }
+
+
+
+addChecker(allCustomOptions)
